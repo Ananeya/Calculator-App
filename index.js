7fb807d027992994fae7935083721c66cf2394bc
Ananeya <ananeyap@gmail.com>
2025-05-14 16:03:14 +0300

    finished the display function

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 "use strict";
 
+const displayInput = document.getElementById("display-input");
+const displayOutput = document.getElementById("display-output");
+
+const clear = document.getElementById("clear");
+const deleteNum = document.getElementById("delete");
+
+function displayNum(input) {
+	displayInput.textContent += input;
+}
